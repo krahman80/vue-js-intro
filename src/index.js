b828d1961b086dd7d01ea76c5183680bd0c48b9f
kautsar rahman <kautsar.rahman@gmail.com>
2026-02-20 18:00:11 +0900

    Loops with v-for

--- src/index.js
+++ src/index.js
@@ -3,24 +3,29 @@ import * as Vue from 'vue/dist/vue.esm-bundler.js'
     template: `
         <button v-on:click="increment()">Increment</button>
         <p>{{ count }}</p>
-        <div v-if="isEven()">
-            Even
-        </div>
-        <div v-else>
-            Odd
+        <div v-for="number in numbers">
+            <div> 
+                {{ number }} 
+                <span v-if="isEven(number)">
+                Even
+                </span>
+                <span v-else>
+                Odd
+                </span> 
+            </div>
         </div>
     `,
     data() {
       return {
-        count: 0
+        numbers: [1,2,3,4,5,6,7,8,9,10]
       }
     },
     methods: {
         increment() {
             this.count += 1
         },
-        isEven() {
-            return this.count % 2 === 0
+        isEven(number) {
+            return number % 2 === 0
         }
     }
   })
