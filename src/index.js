53c743d7b35ae7232f54e1f339841719b6ebaedf
kautsar rahman <kautsar.rahman@gmail.com>
2026-02-21 09:07:05 +0900

    Computed Properties

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import * as Vue from 'vue/dist/vue.esm-bundler.js'
   const app = Vue.createApp({
     template: `
-        <button v-on:click="increment()">Increment</button>
+        <button v-on:click="increment">Increment</button>
         <p>{{ count }}</p>
-        <div v-for="number in numbers">
+        <div v-for="number in evenList">
             <div> 
                 {{ number }} 
-                <span v-if="isEven(number)">
-                Even
-                </span>
-                <span v-else>
-                Odd
-                </span> 
             </div>
         </div>
     `,
     data() {
       return {
+        count: 0,
         numbers: [1,2,3,4,5,6,7,8,9,10]
       }
+    },
+    computed: {
+        evenList() {
+            return this.numbers.filter(num => this.isEven(num))
+        }
     },
     methods: {
         increment() {
