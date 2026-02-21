f9ddbf3e543d7bc7aa19fedd35896de72a18dcba
kautsar rahman <kautsar.rahman@gmail.com>
2026-02-21 09:50:22 +0900

    Input Validation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import * as Vue from 'vue/dist/vue.esm-bundler.js'
     template: `
         <button v-on:click="increment">Increment</button>
         <p>{{ count }}</p>
+        
+        <input 
+            v-bind:value="value"
+            v-on:input="input" 
+        />
+        <div class="red">
+            <p>{{ error }}</p>
+        </div>
+        <p>Loop</p>
         <div 
             v-for="number in numbers"
             v-bind:class="getClass(number)"
@@ -15,15 +24,25 @@ import * as Vue from 'vue/dist/vue.esm-bundler.js'
     data() {
       return {
         count: 0,
-        numbers: [1,2,3,4,5,6,7,8,9,10]
+        value: 'user',
+        numbers: [1,2,3,4,5,6,7,8,9,10],
       }
     },
     computed: {
         evenList() {
             return this.numbers.filter(num => this.isEven(num))
+        },
+        error() {
+            if (this.value.length < 5) {
+                return 'Must be greater than 5'
+            } 
         }
     },
     methods: {
+        input($event) {
+            //console.log($event.target.value)
+            this.value = $event.target.value 
+        },
         getClass(number) {
             return this.isEven(number) ? 'blue' : 'red'
         },
